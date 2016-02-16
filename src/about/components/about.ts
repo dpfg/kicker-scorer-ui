517d27ec5ac33b95d51cb2c13e93e5e475a060c1
Aliaksandr Chaiko <xander.blr@gmail.com>
2016-02-16 22:49:05 +0300

    clean

--- src/about/components/about.ts
+++ src/about/components/about.ts
@@ -1,7 +1,6 @@
 import {Component} from 'angular2/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
 
-import {NameList} from '../../shared/services/name_list';
 
 @Component({
   selector: 'about',
@@ -10,14 +9,5 @@ import {NameList} from '../../shared/services/name_list';
 })
 export class AboutCmp {
   newName: string;
-  constructor(public list: NameList) {}
- /*
- * @param newname  any text as input.
- * @returns return false to prevent default form submit behavior to refresh the page.
- */
-  addName(): boolean {
-    this.list.add(this.newName);
-    this.newName = _.chain([1,2,3,4,5,6,7,8]).filter(x => x % 2 === 0).value().join(',');
-    return false;
-  }
+  constructor() {}
 }
